b0043e1fc8596f2f314a77c94c1bf3a7a4dd556a
wdecoster <decosterwouter@gmail.com>
2023-02-16 00:00:43 +0100

    fix axis labels

--- src/axis_ticks.rs
+++ src/axis_ticks.rs
@@ -1,111 +1,125 @@
 use image::{ImageBuffer, Rgb};
-use imageproc::{drawing::draw_filled_rect, rect::Rect};
+use imageproc::{
+    drawing::{draw_filled_rect, draw_text},
+    rect::Rect,
+};
+use rusttype::{Font, Scale};
 
-use crate::transform::{transform_length, MAX_LENGTH};
+use crate::transform::transform_length;
+
+const COLOR: Rgb<u8> = Rgb([255, 255, 255]);
+const SCALE: Scale = Scale { x: 12.0, y: 12.0 };
 
 pub fn add_ticks(
     mut image: ImageBuffer<Rgb<u8>, Vec<u8>>,
     transform_accuracy: fn(f32) -> usize,
     phred: bool,
 ) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
-    // add major x-axis ticks
-    for tick in &[10, 100, 1000, 10000, 100000] {
-        image = draw_filled_rect(
+    let font_data: &[u8] = include_bytes!("../dev/TimesNewRoman/times new roman.ttf");
+    let font: Font<'static> = Font::try_from_bytes(font_data).expect("Error parsing font file");
+
+    // add major x-axis ticks at the top and bottom, and add axis labels at the bottom
+    for (index, tick) in vec![10, 100, 1000, 10000, 100000].iter().enumerate() {
+        let xcoord = transform_length(*tick) as i32;
+        let pow = 10i32.pow((index + 1).try_into().unwrap());
+        let offset = format!("{tick}").len() as i32;
+        image = draw_filled_rect(&image, Rect::at(xcoord, 0).of_size(1, 6), COLOR);
+        image = draw_filled_rect(&image, Rect::at(xcoord, 294).of_size(1, 6), COLOR);
+        image = draw_text(
             &image,
-            Rect::at(transform_length(*tick) as i32, 0).of_size(1, 6),
-            Rgb([255, 255, 255]),
-        );
+            COLOR,
+            xcoord - 1 - (offset * 2),
+            280,
+            SCALE,
+            &font,
+            &format!("{pow}"),
+        )
     }
-    // adding intermediate x-axis ticks
+
+    // adding intermediate x-axis ticks at the top
     for tick in &[5, 50, 500, 5000, 50000, 500000] {
         image = draw_filled_rect(
             &image,
             Rect::at(transform_length(*tick) as i32, 0).of_size(1, 3),
-            Rgb([255, 255, 255]),
+            COLOR,
         );
     }
-    // adding minor x-axis ticks
+
+    // adding minor x-axis ticks at the top
     for tick in 1..10 {
-        for multiplier in &[1, 10, 100, 1000, 10000, 100000] {
+        for m in &[1, 10, 100, 1000, 10000, 100000] {
             image = draw_filled_rect(
                 &image,
-                Rect::at(transform_length(tick * multiplier) as i32, 0).of_size(1, 1),
-                Rgb([255, 255, 255]),
+                Rect::at(transform_length(tick * m) as i32, 0).of_size(1, 1),
+                COLOR,
             );
         }
     }
+
     if phred {
-        // add major y-axis ticks
-        for tick in &[10, 20, 30, 40] {
-            image = draw_filled_rect(
+        // add major y-axis ticks left and right, and axis labels on the left
+        for tick in &[10, 20, 30] {
+            let ycoord = 300 - (7.5 * (*tick as f32)) as i32;
+            image = draw_filled_rect(&image, Rect::at(294, ycoord).of_size(6, 1), COLOR);
+            image = draw_filled_rect(&image, Rect::at(0, ycoord).of_size(6, 1), COLOR);
+            image = draw_text(
                 &image,
-                Rect::at(
-                    transform_length(MAX_LENGTH) as i32 - 6,
-                    (7.5 * (*tick as f32)) as i32,
-                )
-                .of_size(6, 1),
-                Rgb([255, 255, 255]),
+                COLOR,
+                8,
+                ycoord - 5,
+                SCALE,
+                &font,
+                &format!("Q{tick}"),
             );
         }
+
         // add intermediate y-axis ticks
         for tick in &[5.0, 15.0, 25.0, 35.0] {
             image = draw_filled_rect(
                 &image,
-                Rect::at(
-                    transform_length(MAX_LENGTH) as i32 - 3,
-                    (7.5 * (*tick as f32)) as i32,
-                )
-                .of_size(3, 1),
-                Rgb([255, 255, 255]),
+                Rect::at(297, (7.5 * (*tick as f32)) as i32).of_size(3, 1),
+                COLOR,
             );
         }
+
         // add minor y-axis ticks
         for tick in 0..40 {
             image = draw_filled_rect(
                 &image,
-                Rect::at(
-                    transform_length(MAX_LENGTH) as i32 - 1,
-                    (7.5 * (tick as f32)) as i32,
-                )
-                .of_size(1, 1),
-                Rgb([255, 255, 255]),
+                Rect::at(299, (7.5 * (tick as f32)) as i32).of_size(1, 1),
+                COLOR,
             );
         }
     } else {
-        // add major y-axis ticks
-        for tick in &[70.0, 80.0, 90.0, 100.0] {
-            image = draw_filled_rect(
+        // add major y-axis ticks  ticks left and right, and axis labels on the left
+        for tick in &[80.0, 90.0] {
+            let ycoord = transform_accuracy(*tick) as i32;
+            image = draw_filled_rect(&image, Rect::at(294, ycoord).of_size(6, 1), COLOR);
+            image = draw_filled_rect(&image, Rect::at(0, ycoord).of_size(6, 1), COLOR);
+            image = draw_text(
                 &image,
-                Rect::at(
-                    transform_length(MAX_LENGTH) as i32 - 6,
-                    transform_accuracy(*tick) as i32,
-                )
-                .of_size(6, 1),
-                Rgb([255, 255, 255]),
+                COLOR,
+                8,
+                ycoord - 5,
+                SCALE,
+                &font,
+                &format!("{tick}%"),
             );
         }
         // add intermediate y-axis ticks
         for tick in &[75.0, 85.0, 95.0] {
             image = draw_filled_rect(
                 &image,
-                Rect::at(
-                    transform_length(MAX_LENGTH) as i32 - 3,
-                    transform_accuracy(*tick) as i32,
-                )
-                .of_size(3, 1),
-                Rgb([255, 255, 255]),
+                Rect::at(297, transform_accuracy(*tick) as i32).of_size(3, 1),
+                COLOR,
             );
         }
         // add minor y-axis ticks
         for tick in 70..100 {
             image = draw_filled_rect(
                 &image,
-                Rect::at(
-                    transform_length(MAX_LENGTH) as i32 - 1,
-                    transform_accuracy(tick as f32) as i32,
-                )
-                .of_size(1, 1),
-                Rgb([255, 255, 255]),
+                Rect::at(299, transform_accuracy(tick as f32) as i32).of_size(1, 1),
+                COLOR,
             );
         }
     }
